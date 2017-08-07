c313b2bcc2b51f239abe87ce7d94c437c61a2fdb
Pavel Nezhencev <whyleee@gmail.com>
2017-08-08 01:41:54 +0300

    Add hmr support for vuex modules

--- RDB.Admin/ClientApp/store/store.js
+++ RDB.Admin/ClientApp/store/store.js
@@ -4,8 +4,20 @@ import counter from './counter'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   modules: {
     counter
   }
 })
+
+if (module.hot) {
+  module.hot.accept(['./counter'], () => {
+    store.hotUpdate({
+      modules: {
+        counter: require('./counter').default
+      }
+    })
+  })
+}
+
+export default store
