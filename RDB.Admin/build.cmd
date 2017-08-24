f0e2517a52b8aa5f19e9ae23ed1a78d3a9f2813f
Pavel Nezhencev <whyleee@gmail.com>
2017-08-24 12:09:17 +0300

    Remove rdb prefix from api and admin containers

--- RDB.Admin/build.cmd
+++ RDB.Admin/build.cmd
@@ -1,5 +1,5 @@
 if "%1" == "--production" (
-  docker-compose up -d --no-deps --build rdb.admin
+  docker-compose up -d --no-deps --build admin
 ) else (
-  docker-compose -f ..\docker-compose.yml -f ..\docker-compose.dev.yml up -d --no-deps --build rdb.admin
+  docker-compose -f ..\docker-compose.yml -f ..\docker-compose.dev.yml up -d --no-deps --build admin
 )
--- RDB.Api/build.cmd
+++ RDB.Api/build.cmd
@@ -1,5 +1,5 @@
 if "%1" == "--production" (
-  docker-compose up -d --no-deps --build rdb.api
+  docker-compose up -d --no-deps --build api
 ) else (
-  docker-compose -f ..\docker-compose.yml -f ..\docker-compose.dev.yml up -d --no-deps --build rdb.api
+  docker-compose -f ..\docker-compose.yml -f ..\docker-compose.dev.yml up -d --no-deps --build api
 )
