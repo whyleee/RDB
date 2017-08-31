f67b81def96e3224f4d34a25f873d51e9f456213
Pavel Nezhencev <whyleee@gmail.com>
2017-09-01 00:11:38 +0300

    Add debug containers, setup vscode tasks and debug

--- run.cmd
+++ run.cmd
@@ -3,6 +3,9 @@ if "%1" == "--production" (
   docker-compose -f docker-compose.yml -f docker-compose.production.yml up --build
 ) else if "%1" == "--azure" (
   docker-compose -f docker-compose.yml -f docker-compose.azure.yml up -d --build
+) else if "%1" == "--debug" (
+  set DOCKERFILE_EXT=.debug
+  docker-compose up -d --build
 ) else (
   docker-compose up --build
 )
